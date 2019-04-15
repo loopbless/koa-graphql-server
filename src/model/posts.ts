be2f5a63755a5051031637b2b7bc7f232ecb8319
range <hejialin@veredholdings.com>
2019-04-15 18:03:17 +0800

    fix(posts): fix list posts sql

--- src/model/posts.ts
+++ src/model/posts.ts
@@ -4,23 +4,34 @@ import { Posts } from '../entities/posts';
 import { User } from '../entities/user';
 
 export class PostsDao extends Base<Posts> {
-
   async get(id: number) {
     return await getRepository(Posts)
-    .createQueryBuilder('posts')
-    .leftJoinAndMapOne('posts.user', 'au_user', 'user', 'user.id = posts.createBy')
-    .where('posts.id = :id', {id})
-    .getOne();
+      .createQueryBuilder('posts')
+      .leftJoinAndMapOne(
+        'posts.user',
+        'au_user',
+        'user',
+        'user.id = posts.createBy'
+      )
+      .where('posts.id = :id', { id })
+      .getOne();
   }
 
   async list(offset: number, limit: number) {
-    return await getRepository(Posts)
+    const results = await getRepository(Posts)
       .createQueryBuilder('posts')
-      .leftJoinAndMapMany('posts.user', 'au_user', 'user', 'user.id = posts.createBy')
+      .leftJoinAndMapOne(
+        'posts.user',
+        'au_user',
+        'user',
+        'user.id = posts.createBy'
+      )
       .offset(offset)
       .limit(limit)
-      .getManyAndCount().then(data => {
-        return {totals: data[1], results: data[0]};
-      });
+      .getMany();
+    const totals = await getRepository(Posts)
+      .createQueryBuilder()
+      .getCount();
+    return { totals: totals, results: results };
   }
 }
